feb37199b25084e64fff5134e2ef92ba8fc4511f
bright-tools <dev@brightsilence.com>
2013-11-26 15:48:47 +0000

    Initial ZSH support

---
+++ shell/wd.zsh
@@ -0,0 +1,31 @@
+# wd support in zsh (Z shell)
+
+_wd_complete()
+{
+    reply=(`wd -l`)
+}
+
+wcd()
+{
+    # TODO: Same as bash - needs to be de-duped
+    # If the parameter's a directory, change to it
+    if [ -d "$1" ]; then
+        cd "$1"
+    else
+        # See if the parameter was a bookmark name?
+        if [ "${OSTYPE}" = "cygwin" ]; then
+            # Ensure paths are cygwin formatted
+            local dir=$(wd -n $1 -s c)
+        else
+            local dir=$(wd -n $1)
+        fi
+        if [ -d "${dir}" ]; then
+            cd "${dir}"
+        else
+            echo "wcd: Couldn't find a directory or a bookmark for '$1'";
+        fi
+    fi
+
+}
+
+compctl -K _wd_complete wcd
