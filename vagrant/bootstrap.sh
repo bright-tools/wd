612bd57e3129327da4c9284ef13a3dc603604ef5
bright-tools <dev@brightsilence.com>
2018-04-13 17:35:46 +0100

    Use bundle to install Aruba

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -2,4 +2,14 @@
 
 sudo apt-get update
 sudo apt-get install -y gcc make tmux zsh
-sudo gem install aruba bundle
+
+if [ -z "$TRAVIS"]; then
+    sudo apt-get install -y ruby2.3 ruby2.3-dev
+fi
+
+# Change directory to the checkout root
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/..
+
+sudo gem install bundle
+bundle install
