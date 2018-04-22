442fda66b8cb0ebd0da2df0b9b2e4ad8285f56a2
bright-tools <dev@brightsilence.com>
2018-04-22 16:19:50 +0100

    Add filter for @onlywindows

--- features/support/filter.rb
+++ features/support/filter.rb
@@ -4,10 +4,13 @@ require 'rbconfig'
 # Filter out tests which don't run on Windows
 WindowsFilter = Cucumber::Core::Filter.new do
     def test_case(test_case)
-        is_not_windows = !(RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/i)
-        is_not_tagged = !test_case.match_tags?("@notwindows")
+        is_not_windows_platform = !(RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/i)
+        is_not_windows_test = !test_case.match_tags?("@notwindows")
+        is_windows_only_test = test_case.match_tags?("@onlywindows")
 
-        if is_not_windows || is_not_tagged then
+        if is_not_windows_platform && is_windows_only_test then
+            # 
+        elsif is_not_windows_platform || is_not_windows_test then
             test_case.describe_to(receiver)
         end
     end
